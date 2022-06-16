c42f4963a068179422703c25f637261911ad06bd
ricardojosino <ricardojosino@gmail.com>
2022-06-16 15:40:25 -0300

    Aplicado negrito nas palavras chaves

---
+++ src/asset/js/script.js
@@ -0,0 +1,30 @@
+function playVideo(selector_video, selector_button, video_id, player) {
+    let video = document.querySelector(selector_video)
+    let video_click = document.querySelector(selector_button)
+    let vimeo, youtube, src
+
+
+    video_click.addEventListener('click', function (e) {
+        e.preventDefault()
+
+        vimeo = `<iframe class="w-full aspect-video" src="https://player.vimeo.com/video/${video_id}?h=bc18484ce9&autoplay=1&color=6666FF&title=0&byline=0&portrait=0" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>`;
+        youtube = `<iframe class="w-full aspect-video" src="https://www.youtube.com/embed/${video_id}?autoplay=1&modestbranding=1&rel=0&showinfo=0&controls=2&fs=1" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+
+        if (player === 'youtube') {
+            src = youtube
+        } else if (player === 'vimeo') {
+            src = vimeo
+        } else {
+            src = 'Não possível reproduzir o vídeo'
+        }
+
+        video.innerHTML = src
+
+
+    })
+}
+
+// hcitt97R1Vk
+// quYodSv0Wr0
+playVideo('#video-venda', '#video-venda-click', '718436952', 'vimeo')
+playVideo('#video-conheca-shantala', '#video-conheca-shantala-click', '718436952', 'vimeo')
